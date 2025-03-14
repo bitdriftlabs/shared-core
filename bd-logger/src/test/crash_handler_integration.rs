4d6cb81e1eaa9db93779cabde0fc1e685aa36231
Snow Pettersen <snow@bitdrift.io>
2025-03-14 09:50:56 -0700

    extract timestamp from start of crash artifact (#117)

--- bd-logger/src/test/crash_handler_integration.rs
+++ bd-logger/src/test/crash_handler_integration.rs
@@ -13,9 +13,12 @@ use bd_runtime::runtime::crash_handling::CrashDirectories;
 use bd_runtime::runtime::FeatureFlag;
 use bd_test_helpers::runtime::{make_simple_update, ValueKind};
 use time::ext::{NumericalDuration, NumericalStdDuration};
+use time::macros::datetime;
 
 #[test]
 fn crash_reports() {
+  let timestamp = datetime!(2021-01-01 00:00:00 UTC);
+
   let (directory, initial_session_id) = {
     let setup = Setup::new_with_options(SetupOptions {
       disk_storage: true,
@@ -27,6 +30,16 @@ fn crash_reports() {
     std::fs::write(
       setup.sdk_directory.path().join("reports/new/crash1.txt"),
       "crash1",
+    )
+    .unwrap();
+
+
+    std::fs::write(
+      setup.sdk_directory.path().join(format!(
+        "reports/new/{}_crash2.txt",
+        timestamp.unix_timestamp()
+      )),
+      "crash2",
     )
     .unwrap();
 
@@ -51,11 +64,18 @@ fn crash_reports() {
   setup.upload_individual_logs();
 
   assert_matches!(setup.server.blocking_next_log_upload(), Some(upload) => {
-    assert_eq!(upload.logs().len(), 1);
-    assert_eq!(upload.logs()[0].message(), "App crashed");
-    assert_eq!(upload.logs()[0].message(), "App crashed");
-    assert_eq!(upload.logs()[0].binary_field("_crash_artifact"), b"crash1");
-    assert_eq!(upload.logs()[0].session_id(), initial_session_id);
+    assert_eq!(upload.logs().len(), 2);
+
+    let logs = upload.logs();
+    let crash1 = logs.iter().find(|log| log.binary_field("_crash_artifact") == b"crash1").unwrap();
+    assert_eq!(crash1.message(), "App crashed");
+    assert_eq!(crash1.session_id(), initial_session_id);
+    assert_ne!(crash1.timestamp(), timestamp);
+
+    let crash2 = logs.iter().find(|log| log.binary_field("_crash_artifact") == b"crash2").unwrap();
+    assert_eq!(crash2.message(), "App crashed");
+    assert_eq!(crash2.session_id(), initial_session_id);
+    assert_eq!(crash2.timestamp(), timestamp);
   });
 }
 
