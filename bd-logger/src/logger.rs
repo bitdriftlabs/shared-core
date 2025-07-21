b7171f8cd09f08cf59888fd4a8956b8cd855d420
Delisa <kattrali@users.noreply.github.com>
2025-07-21 15:05:03 +0100

    increment app_open at logger init (#197)
    * increment app_open at logger init
    
    consider the creation of the stat to be concurrent with app open
    
    * remove helper func for incrementing app-open counter

--- bd-logger/src/logger.rs
+++ bd-logger/src/logger.rs
@@ -199,10 +199,6 @@ impl LoggerHandle {
     });
   }
 
-  pub fn record_app_open(&self) {
-    self.stats.app_open.inc();
-  }
-
   pub fn log_resource_utilization(&self, mut fields: AnnotatedLogFields, duration: time::Duration) {
     fields.insert(
       "_duration_ms".into(),
@@ -500,7 +496,7 @@ impl Logger {
   pub fn new(
     shutdown_state: Option<ComponentShutdownTrigger>,
     runtime_loader: Arc<bd_runtime::runtime::ConfigLoader>,
-    stats: Scope,
+    stats_scope: Scope,
     async_log_buffer_tx: MemoryBoundSender<AsyncLogBufferMessage>,
     session_strategy: Arc<bd_session::Strategy>,
     device: Arc<bd_device::Device>,
@@ -508,6 +504,11 @@ impl Logger {
     store: Arc<bd_key_value::Store>,
     sleep_mode_active: watch::Sender<bool>,
   ) -> Self {
+    let stats = Stats::new(&stats_scope);
+
+    // record initial app open for this launch's logger
+    stats.app_open.inc();
+
     Self {
       shutdown_state: Mutex::new(shutdown_state),
       session_strategy,
@@ -515,8 +516,8 @@ impl Logger {
       sdk_version: sdk_version.to_string(),
       runtime_loader,
       async_log_buffer_tx,
-      stats: Stats::new(&stats),
-      stats_scope: stats,
+      stats,
+      stats_scope,
       store,
       sleep_mode_active,
     }
