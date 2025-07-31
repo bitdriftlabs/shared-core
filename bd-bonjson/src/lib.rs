5d3c3d5db17eb7ae1a1f12dee0f3b9ede2afa479
Matt Klein <mklein@bitdrift.io>
2025-07-31 09:16:35 -0600

    changes
    Signed-off-by: Matt Klein <mklein@bitdrift.io>

--- bd-bonjson/src/lib.rs
+++ bd-bonjson/src/lib.rs
@@ -1,22 +1,30 @@
-pub mod error;
-pub mod ser;
 pub mod de;
+#[cfg(test)]
+pub mod ser;
 pub mod type_codes;
 
 mod primitives;
 mod writer;
 
 pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+  left + right
 }
+
+pub enum Error {
+  InvalidSerialization,
+  InvalidDeserialization,
+  Io { offset: u64 },
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+  #[test]
+  fn it_works() {
+    let result = add(2, 2);
+    assert_eq!(result, 4);
+  }
 }
