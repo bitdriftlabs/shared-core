08f640130e4fd35c98206cd39cef188efda81c70
Karl Stenerud <kstenerud@gmail.com>
2025-08-05 13:16:47 +0200

    cleanup

--- bd-bonjson/src/decoder.rs
+++ bd-bonjson/src/decoder.rs
@@ -197,7 +197,7 @@ impl<'a> Decoder<'a> {
         if e.value != Value::None {
           elements.push(e.value.clone());
         }
-        self.propagate_error(e, Value::Array(elements.clone()))
+        propagate_error(e, Value::Array(elements.clone()))
       })?;
 
       elements.push(value);
@@ -227,14 +227,14 @@ impl<'a> Decoder<'a> {
             Value::Object(object.clone()),
           ));
         },
-        Err(e) => return Err(self.propagate_error(e, Value::Object(object.clone()))),
+        Err(e) => return Err(propagate_error(e, Value::Object(object.clone()))),
       };
 
       let value = self.decode_value().map_err(|e| {
         if e.value != Value::None {
           object.insert(key.clone(), e.value.clone());
         }
-        self.propagate_error(e, Value::Object(object.clone()))
+        propagate_error(e, Value::Object(object.clone()))
       })?;
 
       object.insert(key, value);
