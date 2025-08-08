3679a309c398e220ec30f375976ccb61955f2398
Karl Stenerud <kstenerud@gmail.com>
2025-08-08 11:41:10 +0200

    cleanup

--- fuzz/fuzz_targets/bonjson.rs
+++ fuzz/fuzz_targets/bonjson.rs
@@ -13,7 +13,6 @@ use libfuzzer_sys::fuzz_target;
 fuzz_target!(|data: &[u8]| {
   let mut decoder = Decoder::new(data);
   match decoder.decode() {
-    Ok(_) => {},
-    Err(_) => {},
+    Ok(_) | Err(_) => {},
   }
 });
