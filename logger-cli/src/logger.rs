c7f242709f818cfc56324e5811704a65446902f6
delisa fuller <dfuller@bitdrift.io>
2025-08-12 15:08:43 +0100

    feat: add rudimentary logger runner

---
+++ logger-cli/src/logger.rs
@@ -0,0 +1,138 @@
+// shared-core - bitdrift's common client/server libraries
+// Copyright Bitdrift, Inc. All rights reserved.
+//
+// Use of this source code is governed by a source available license that can be found in the
+// LICENSE file or at:
+// https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt
+
+use crate::cli::{FieldPairs, LogCommand};
+use crate::storage::SQLiteStorage;
+use bd_logger::{CaptureSession, InitParams, Logger};
+use bd_session::{Strategy, fixed};
+use bd_test_helpers::metadata::EmptyMetadata;
+use bd_test_helpers::metadata_provider::LogMetadata;
+use parking_lot::Mutex;
+use std::path::Path;
+use std::pin::Pin;
+use std::sync::Arc;
+use std::thread::sleep;
+use time::ext::NumericalStdDuration;
+
+pub type LoggerFuture =
+  Pin<Box<dyn Future<Output = anyhow::Result<()>> + 'static + std::marker::Send>>;
+
+pub struct LoggerHolder {
+  logger: Logger,
+  future: Mutex<Option<LoggerFuture>>,
+}
+
+impl LoggerHolder {
+  pub fn new(logger: Logger, future: LoggerFuture) -> Self {
+    Self {
+      logger,
+      future: Mutex::new(Some(future)),
+    }
+  }
+
+  pub fn start(&self) {
+    let Some(future) = self.future.lock().take() else {
+      return;
+    };
+
+    sleep(2.std_seconds()); // some initialization time for the network
+
+    std::thread::spawn(move || {
+      tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+          if let Err(e) = future.await {
+            log::error!("Failed to start logger: {e}");
+          }
+        });
+    });
+  }
+
+  pub fn process_crash_reports(&mut self) -> anyhow::Result<()> {
+    self
+      .logger
+      .process_crash_reports(bd_logger::ReportProcessingSession::PreviousRun)
+  }
+
+  pub fn stop(&self) {
+    sleep(2.std_seconds());
+    self.logger.shutdown(true);
+  }
+
+  pub fn log(&self, cmd: LogCommand, capture_session: bool) {
+    let session_capture = if capture_session {
+      CaptureSession::capture_with_id("cli command")
+    } else {
+      CaptureSession::default()
+    };
+    self.logger.new_logger_handle().log(
+      cmd.log_level.into(),
+      cmd.log_type.into(),
+      cmd.message.into(),
+      FieldPairs(cmd.field).into(),
+      [].into(),
+      None,
+      bd_logger::Block::Yes,
+      session_capture,
+    );
+  }
+}
+
+struct MaybeStaticSessionGenerator {
+  session_id: Option<String>,
+}
+
+impl fixed::Callbacks for MaybeStaticSessionGenerator {
+  fn generate_session_id(&self) -> anyhow::Result<String> {
+    self.session_id.as_ref().map_or_else(
+      || fixed::UUIDCallbacks.generate_session_id(),
+      |id| Ok(id.clone()),
+    )
+  }
+}
+
+pub fn make_logger(
+  sdk_directory: &Path,
+  api_key: String,
+  api_url: String,
+  session_id: Option<String>,
+) -> anyhow::Result<LoggerHolder> {
+  let session_callbacks = Arc::new(MaybeStaticSessionGenerator { session_id });
+  let storage_db = sdk_directory.join("defaults.db");
+  let storage = SQLiteStorage::new(&storage_db);
+  let store = Arc::new(bd_key_value::Store::new(Box::new(storage)));
+  let device = Arc::new(bd_device::Device::new(store.clone()));
+  let shutdown_trigger = bd_shutdown::ComponentShutdownTrigger::default();
+  let shutdown = shutdown_trigger.make_shutdown();
+  let network = bd_hyper_network::HyperNetwork::run_on_thread(&api_url, shutdown);
+
+  let (logger, _, future, _) = bd_logger::LoggerBuilder::new(InitParams {
+    sdk_directory: sdk_directory.to_path_buf(),
+    api_key,
+    session_strategy: Arc::new(Strategy::Fixed(fixed::Strategy::new(
+      store.clone(),
+      session_callbacks,
+    ))),
+    metadata_provider: Arc::new(LogMetadata {
+      // TODO: app id, version, platform
+      timestamp: time::OffsetDateTime::now_utc().into(),
+      ..Default::default()
+    }),
+    resource_utilization_target: Box::new(bd_test_helpers::resource_utilization::EmptyTarget),
+    session_replay_target: Box::new(bd_test_helpers::session_replay::NoOpTarget),
+    events_listener_target: Box::new(bd_test_helpers::events::NoOpListenerTarget),
+    device,
+    store,
+    network: Box::new(network),
+    static_metadata: Arc::new(EmptyMetadata),
+    start_in_sleep_mode: false,
+  })
+  .build()?;
+  Ok(LoggerHolder::new(logger, future))
+}
---
+++ logger-cli/src/main.rs
@@ -0,0 +1,55 @@
+// shared-core - bitdrift's common client/server libraries
+// Copyright Bitdrift, Inc. All rights reserved.
+//
+// Use of this source code is governed by a source available license that can be found in the
+// LICENSE file or at:
+// https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt
+
+use crate::cli::Command;
+use bd_log::SwapLogger;
+use clap::Parser;
+use std::env;
+use std::path::Path;
+
+mod cli;
+mod logger;
+mod storage;
+
+fn main() -> anyhow::Result<()> {
+  // initialize console logging
+  SwapLogger::initialize();
+  let args = crate::cli::Options::parse();
+
+  let home = env::var("HOME")?;
+  let sdk_directory = Path::new(&home).join(".local").join("bd-logger-cli");
+  std::fs::create_dir_all(&sdk_directory)?;
+
+  let mut logger =
+    crate::logger::make_logger(&sdk_directory, args.api_key, args.api_url, args.session_id)?;
+
+  match args.command {
+    Command::EnqueueArtifacts(cmd) => {
+      let report_dir = &sdk_directory.join("reports/new");
+      std::fs::create_dir_all(report_dir)?;
+      for path in &cmd.path {
+        let source_path = Path::new(&path);
+        std::fs::copy(
+          source_path,
+          report_dir.join(source_path.file_name().unwrap()),
+        )?;
+      }
+    },
+    Command::UploadArtifacts => {
+      logger.start();
+      logger.process_crash_reports()?;
+      logger.stop();
+    },
+    Command::Log(cmd) => {
+      logger.start();
+      logger.log(cmd, true);
+      logger.stop();
+    },
+  }
+
+  Ok(())
+}
