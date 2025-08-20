8f02af3132d13f5e42fc1fca1d82a04fc5a5622c
delisa fuller <dfuller@bitdrift.io>
2025-08-20 11:42:37 +0100

    refactor: remove unneeded contextual hints

--- bd-report-parsers/src/lib.rs
+++ bd-report-parsers/src/lib.rs
@@ -7,17 +7,13 @@
 
 use nom::character::complete::one_of;
 use nom::combinator::{map, recognize};
-use nom::error::{ContextError, ParseError};
+use nom::error::ParseError;
 use nom::multi::many1;
 use nom::{IResult, Parser};
 
 pub mod android;
 
-fn decimal<
-  'a,
-  T: std::str::FromStr + std::default::Default,
-  E: ParseError<&'a str> + ContextError<&'a str>,
->(
+fn decimal<'a, T: std::str::FromStr + std::default::Default, E: ParseError<&'a str>>(
   input: &'a str,
 ) -> IResult<&'a str, T, E> {
   map(recognize(many1(one_of("0123456789"))), |out: &str| {
@@ -26,9 +22,7 @@ fn decimal<
   .parse(input)
 }
 
-fn hexadecimal<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
-  input: &'a str,
-) -> IResult<&'a str, u64, E> {
+fn hexadecimal<'a, E: ParseError<&'a str>>(input: &'a str) -> IResult<&'a str, u64, E> {
   map(
     recognize(many1(one_of("0123456789abcdefABCDEF"))),
     |out: &str| u64::from_str_radix(out, 16).unwrap_or_default(),
