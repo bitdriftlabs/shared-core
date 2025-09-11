fdc0ac53d1f690e97d349dca2235296827d52a97
Snow Pettersen <snow@bitdrift.io>
2025-09-11 15:22:11 -0600

    bump api to bring in feature flag timestamp type change

--- bd-proto/src/flatbuffers/common_generated.rs
+++ bd-proto/src/flatbuffers/common_generated.rs
@@ -531,6 +531,120 @@ impl core::fmt::Debug for Field<'_> {
       ds.finish()
   }
 }
+pub enum TimestampOffset {}
+#[derive(Copy, Clone, PartialEq)]
+
+pub struct Timestamp<'a> {
+  pub _tab: flatbuffers::Table<'a>,
+}
+
+impl<'a> flatbuffers::Follow<'a> for Timestamp<'a> {
+  type Inner = Timestamp<'a>;
+  #[inline]
+  unsafe fn follow(buf: &'a [u8], loc: usize) -> Self::Inner {
+    Self { _tab: flatbuffers::Table::new(buf, loc) }
+  }
+}
+
+impl<'a> Timestamp<'a> {
+  pub const VT_SECONDS: flatbuffers::VOffsetT = 4;
+  pub const VT_NANOS: flatbuffers::VOffsetT = 6;
+
+  #[inline]
+  pub unsafe fn init_from_table(table: flatbuffers::Table<'a>) -> Self {
+    Timestamp { _tab: table }
+  }
+  #[allow(unused_mut)]
+  pub fn create<'bldr: 'args, 'args: 'mut_bldr, 'mut_bldr, A: flatbuffers::Allocator + 'bldr>(
+    _fbb: &'mut_bldr mut flatbuffers::FlatBufferBuilder<'bldr, A>,
+    args: &'args TimestampArgs
+  ) -> flatbuffers::WIPOffset<Timestamp<'bldr>> {
+    let mut builder = TimestampBuilder::new(_fbb);
+    builder.add_seconds(args.seconds);
+    builder.add_nanos(args.nanos);
+    builder.finish()
+  }
+
+
+  #[inline]
+  pub fn seconds(&self) -> i64 {
+    // Safety:
+    // Created from valid Table for this object
+    // which contains a valid value in this slot
+    unsafe { self._tab.get::<i64>(Timestamp::VT_SECONDS, Some(0)).unwrap()}
+  }
+  #[inline]
+  pub fn nanos(&self) -> i32 {
+    // Safety:
+    // Created from valid Table for this object
+    // which contains a valid value in this slot
+    unsafe { self._tab.get::<i32>(Timestamp::VT_NANOS, Some(0)).unwrap()}
+  }
+}
+
+impl flatbuffers::Verifiable for Timestamp<'_> {
+  #[inline]
+  fn run_verifier(
+    v: &mut flatbuffers::Verifier, pos: usize
+  ) -> Result<(), flatbuffers::InvalidFlatbuffer> {
+    use self::flatbuffers::Verifiable;
+    v.visit_table(pos)?
+     .visit_field::<i64>("seconds", Self::VT_SECONDS, false)?
+     .visit_field::<i32>("nanos", Self::VT_NANOS, false)?
+     .finish();
+    Ok(())
+  }
+}
+pub struct TimestampArgs {
+    pub seconds: i64,
+    pub nanos: i32,
+}
+impl<'a> Default for TimestampArgs {
+  #[inline]
+  fn default() -> Self {
+    TimestampArgs {
+      seconds: 0,
+      nanos: 0,
+    }
+  }
+}
+
+pub struct TimestampBuilder<'a: 'b, 'b, A: flatbuffers::Allocator + 'a> {
+  fbb_: &'b mut flatbuffers::FlatBufferBuilder<'a, A>,
+  start_: flatbuffers::WIPOffset<flatbuffers::TableUnfinishedWIPOffset>,
+}
+impl<'a: 'b, 'b, A: flatbuffers::Allocator + 'a> TimestampBuilder<'a, 'b, A> {
+  #[inline]
+  pub fn add_seconds(&mut self, seconds: i64) {
+    self.fbb_.push_slot::<i64>(Timestamp::VT_SECONDS, seconds, 0);
+  }
+  #[inline]
+  pub fn add_nanos(&mut self, nanos: i32) {
+    self.fbb_.push_slot::<i32>(Timestamp::VT_NANOS, nanos, 0);
+  }
+  #[inline]
+  pub fn new(_fbb: &'b mut flatbuffers::FlatBufferBuilder<'a, A>) -> TimestampBuilder<'a, 'b, A> {
+    let start = _fbb.start_table();
+    TimestampBuilder {
+      fbb_: _fbb,
+      start_: start,
+    }
+  }
+  #[inline]
+  pub fn finish(self) -> flatbuffers::WIPOffset<Timestamp<'a>> {
+    let o = self.fbb_.end_table(self.start_);
+    flatbuffers::WIPOffset::new(o.value())
+  }
+}
+
+impl core::fmt::Debug for Timestamp<'_> {
+  fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+    let mut ds = f.debug_struct("Timestamp");
+      ds.field("seconds", &self.seconds());
+      ds.field("nanos", &self.nanos());
+      ds.finish()
+  }
+}
 }  // pub mod v1
 }  // pub mod common
 }  // pub mod fbs
