4548a054c64c81e2783a0fade2571790db993202
Snow Pettersen <snow@bitdrift.io>
2026-03-25 09:15:43 -0700

    update protobuf + remove annotation related helpers

--- bd-proto-util/src/lib.rs
+++ bd-proto-util/src/lib.rs
@@ -5,7 +5,6 @@
 // LICENSE file or at:
 // https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt
 
-pub mod annotations;
 pub mod fbs;
 pub mod proto;
 pub mod serialization;
