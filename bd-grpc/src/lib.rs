49299db72211ca7f1e1b98698900e3179818821c
Snow Pettersen <snow@bitdrift.io>
2026-04-16 09:27:58 -0700

    fmt

--- bd-grpc/src/lib.rs
+++ bd-grpc/src/lib.rs
@@ -225,11 +225,15 @@ impl<IncomingType: DecodingResult> StreamingApiReceiver<IncomingType> {
     read_stop: Option<watch::Receiver<bool>>,
   ) -> Self {
     let decompression = if headers
-      .get(GRPC_ENCODING_HEADER).as_ref().is_some_and(|v| *v == GRPC_ENCODING_DEFLATE)
+      .get(GRPC_ENCODING_HEADER)
+      .as_ref()
+      .is_some_and(|v| *v == GRPC_ENCODING_DEFLATE)
     {
       Some(bd_grpc_codec::Decompression::StatelessZlib)
     } else if headers
-      .get(LEGACY_GRPC_ENCODING_HEADER).as_ref().is_some_and(|v| *v == GRPC_ENCODING_DEFLATE)
+      .get(LEGACY_GRPC_ENCODING_HEADER)
+      .as_ref()
+      .is_some_and(|v| *v == GRPC_ENCODING_DEFLATE)
     {
       // This is not to spec but is kept around for legacy clients.
       // TODO(mattklein123): Add a metric for this.
