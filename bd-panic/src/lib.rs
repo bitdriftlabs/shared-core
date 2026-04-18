61c9d916688296cd621ee97cf9337fc6a887c886
Matt Klein <mattklein123@gmail.com>
2026-04-17 20:21:16 -0600

    panic: fix LOG_PANIC=1 message to work with &str/String (#457)
    Signed-off-by: Matt Klein <mklein@bitdrift.io>

--- bd-panic/src/lib.rs
+++ bd-panic/src/lib.rs
@@ -28,14 +28,14 @@ fn platform_default() {
   // we want panics to be on a single log line for easy searching.
   if std::env::var("LOG_PANIC").is_ok() {
     std::panic::set_hook(Box::new(move |info| {
-      let message = info.payload().downcast_ref::<String>().map_or_else(
+      let message = info.payload_as_str().map_or_else(
         || {
-          info
-            .payload()
-            .downcast_ref::<String>()
-            .map_or("<none>", |s| s.as_str())
+          format!(
+            "<non-string panic payload type_id={:?}>",
+            info.payload().type_id(),
+          )
         },
-        |s| s.as_str(),
+        str::to_owned,
       );
 
       let location = info.location().map_or_else(
