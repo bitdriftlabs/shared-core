f8a7b4e8539330c03641d2bd79ec1e27d4b25b47
Delisa <kattrali@users.noreply.github.com>
2026-05-19 14:22:50 +0100

    reports: add scripting engine (#479)
    Signed-off-by: Matt Klein <mklein@bitdrift.io>
    Co-authored-by: Matt Klein <mklein@bitdrift.io>

---
+++ bd-script/src/report/functions/set_grouping_key.rs
@@ -0,0 +1,94 @@
+// shared-core - bitdrift's common client/server libraries
+// Copyright Bitdrift, Inc. All rights reserved.
+//
+// Use of this source code is governed by a source available license that can be found in the
+// LICENSE file or at:
+// https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt
+
+use crate::get_dynamic_data;
+use crate::report::ReportOutput;
+use vrl::prelude::{
+  Expression,
+  ExpressionError,
+  Function,
+  FunctionExpression,
+  Parameter,
+  TypeDef,
+  kind,
+};
+
+#[derive(Debug)]
+pub struct SetGroupingKey;
+
+impl Function for SetGroupingKey {
+  fn identifier(&self) -> &'static str {
+    "set_grouping_key"
+  }
+
+  fn examples(&self) -> &'static [vrl::prelude::Example] {
+    &[]
+  }
+
+  fn usage(&self) -> &'static str {
+    ""
+  }
+
+  fn category(&self) -> &'static str {
+    ""
+  }
+
+  fn return_kind(&self) -> u16 {
+    kind::NULL
+  }
+
+  fn parameters(&self) -> &'static [Parameter] {
+    &[Parameter {
+      keyword: "key",
+      kind: kind::BYTES | kind::NULL,
+      required: true,
+      description: "",
+      default: None,
+      enum_variants: None,
+    }]
+  }
+
+  fn compile(
+    &self,
+    _state: &vrl::prelude::TypeState,
+    _ctx: &mut vrl::prelude::FunctionCompileContext,
+    arguments: vrl::prelude::ArgumentList,
+  ) -> vrl::prelude::Compiled {
+    Ok(
+      GroupingKey {
+        key: arguments.required("key"),
+      }
+      .as_expr(),
+    )
+  }
+}
+
+#[derive(Clone, Debug)]
+struct GroupingKey {
+  key: Box<dyn Expression>,
+}
+
+impl FunctionExpression for GroupingKey {
+  fn resolve(&self, ctx: &mut vrl::prelude::Context<'_>) -> vrl::prelude::Resolved {
+    let key = self.key.resolve(ctx)?;
+    if !key.is_bytes() && !key.is_null() {
+      return Err(ExpressionError::from(
+        "grouping key must be a string value or null",
+      ));
+    }
+    let Some(data) = get_dynamic_data::<ReportOutput>(ctx) else {
+      return Ok(key);
+    };
+
+    data.grouping_hints.grouping_key = key.as_str().as_deref().map(ToString::to_string);
+    Ok(key)
+  }
+
+  fn type_def(&self, _state: &vrl::prelude::TypeState) -> TypeDef {
+    TypeDef::bytes().infallible().impure()
+  }
+}
---
+++ bd-script/src/target.rs
@@ -0,0 +1,64 @@
+// shared-core - bitdrift's common client/server libraries
+// Copyright Bitdrift, Inc. All rights reserved.
+//
+// Use of this source code is governed by a source available license that can be found in the
+// LICENSE file or at:
+// https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt
+
+use crate::input::Scriptable;
+use vrl::compiler::{OwnedValueOrRef, SecretTarget, Target};
+use vrl::core::Value;
+use vrl::path::{OwnedTargetPath, PathPrefix};
+
+pub struct ScriptableTarget<'a, T: Scriptable> {
+  object: &'a T,
+}
+
+impl<T: Scriptable> std::fmt::Debug for ScriptableTarget<'_, T> {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    f.debug_struct("ReportTarget").finish()
+  }
+}
+
+impl<'a, T: Scriptable> ScriptableTarget<'a, T> {
+  pub fn new(report: &'a T) -> Self {
+    Self { object: report }
+  }
+}
+
+impl<T: Scriptable> SecretTarget for ScriptableTarget<'_, T> {
+  fn get_secret(&self, _key: &str) -> Option<&str> {
+    None
+  }
+
+  fn insert_secret(&mut self, _key: &str, _value: &str) {}
+
+  fn remove_secret(&mut self, _key: &str) {}
+}
+
+impl<T: Scriptable> Target for ScriptableTarget<'_, T> {
+  fn target_insert(&mut self, _path: &OwnedTargetPath, _value: Value) -> Result<(), String> {
+    Ok(())
+  }
+
+  fn target_get(
+    &mut self,
+    path: &vrl::path::OwnedTargetPath,
+  ) -> Result<Option<vrl::compiler::OwnedValueOrRef<'_>>, String> {
+    if path.prefix != PathPrefix::Event {
+      return Ok(None);
+    }
+    match self.object.resolve(&path.path.segments[..]) {
+      Ok(value) => Ok(value.map(|v| OwnedValueOrRef::Owned(v.0))),
+      Err(path_err) => Err(path_err.to_string()),
+    }
+  }
+
+  fn target_remove(
+    &mut self,
+    _path: &vrl::path::OwnedTargetPath,
+    _compact: bool,
+  ) -> Result<Option<vrl::prelude::Value>, String> {
+    Ok(None)
+  }
+}
