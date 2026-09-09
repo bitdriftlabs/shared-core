3932e740baffefbf0c4a827c37892cf34fe27758
Matt Klein <mklein@bitdrift.io>
2026-09-09 14:22:22 -0600

    fix
    Signed-off-by: Matt Klein <mklein@bitdrift.io>

--- bd-proto/src/protos/state/mod.rs
+++ bd-proto/src/protos/state/mod.rs
@@ -4,9 +4,11 @@ source_info_gated_mod! {
   scope => "with_source/scope.rs",
 }
 
+// The generated state payload imports logging's `payload.proto` as `super::payload`.
+// Keep the legacy state namespace while routing that import's descriptor to logging.
 pub mod payload {
-  pub use super::state_payload::*;
-  pub use crate::protos::logging::payload::Data;
+  pub use super::state_payload::{StateValue, state_value};
+  pub use crate::protos::logging::payload::{Data, file_descriptor};
 }
 
 use super::value_matcher::value_matcher;
